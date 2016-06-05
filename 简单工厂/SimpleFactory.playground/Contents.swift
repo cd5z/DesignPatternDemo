5a52caca108199cc7a976eb5ad74ab81063d386f
codercd <codercd@163.com>
2016-06-05 17:24:05 +0800

    protocol & struct

--- 简单工厂/SimpleFactory.playground/Contents.swift
+++ 简单工厂/SimpleFactory.playground/Contents.swift
@@ -4,46 +4,55 @@ import UIKit
 
 //MARK: Simple Factory
 
-class Operation {
+protocol Operation {
+    var numberA: Double {set get}
+    var numberB: Double {set get}
+    
+    func getResult()->Double
+}
+
+struct OperationAdd: Operation {
     var numberA = 0.0
     var numberB = 0.0
     
-    func getResult()->Double{
-        let result:Double = 0.0
-        return result
-    }
-}
-
-class OperationAdd: Operation {
-    override func getResult() -> Double {
+    func getResult() -> Double {
         return numberA + numberB
     }
 }
 
 
-class OperationSub: Operation {
-    override func getResult() -> Double {
+struct OperationSub: Operation {
+    var numberA = 0.0
+    var numberB = 0.0
+    
+    func getResult() -> Double {
         return numberA - numberB
     }
 }
 
-class OperationMul: Operation {
-    override func getResult() -> Double {
+struct OperationMul: Operation {
+    var numberA = 0.0
+    var numberB = 0.0
+    
+    func getResult() -> Double {
         return numberA * numberB
     }
 }
 
-class OperationDiv: Operation {
-    override func getResult() -> Double {
+struct OperationDiv: Operation {
+    var numberA = 0.0
+    var numberB = 0.0
+    
+    func getResult() -> Double {
         guard numberB != 0 else { print("除数不能为0"); return 0 }
         return numberA / numberB
     }
 }
 
 
-class OperationFactory {
-    class func creatOperation(operation:String) -> Operation {
-        var oper:Operation = Operation()
+struct OperationFactory {
+    static func creatOperation(operation:String) -> Operation {
+        var oper:Operation = OperationAdd()
         switch operation {
         case "+":
             oper = OperationAdd()
@@ -60,8 +69,7 @@ class OperationFactory {
     }
 }
 
-let oper = OperationFactory.creatOperation("+")
-
+var oper = OperationFactory.creatOperation("+")
 oper.numberA = 1
 oper.numberB = 2
 
